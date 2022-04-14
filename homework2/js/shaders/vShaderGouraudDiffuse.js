6a11a4bd43ba8fb71c2a66c5e0cc2bdf9890eb17
Kevin Tan <33914892+kevtan@users.noreply.github.com>
2022-04-14 02:39:32 -0700

    ✨ Finish 2.1.1.1

--- homework2/js/shaders/vShaderGouraudDiffuse.js
+++ homework2/js/shaders/vShaderGouraudDiffuse.js
@@ -60,7 +60,16 @@ void main() {
 	// Compute ambient reflection
 	vec3 ambientReflection = material.ambient * ambientLightColor;
 
-	vColor = ambientReflection;
+	// Compute diffuse reflection
+	vec3 normalInViewSpace = normalize(normalMat * normal);
+	vec3 lightInViewSpace = (viewMat * vec4(pointLights[0].position, 1.0)).xyz;
+	vec3 positionInViewSpace = (modelViewMat * vec4(position, 1.0)).xyz;
+	vec3 displacementInViewSpace = lightInViewSpace - positionInViewSpace;
+	vec3 L = normalize(displacementInViewSpace);
+	float angularAdjustment = max(dot(L, normalInViewSpace), 0.0);
+	vec3 diffuseReflection = (material.diffuse * pointLights[0].color) * angularAdjustment;
+
+	vColor = ambientReflection + diffuseReflection;
 
 	gl_Position =
 		projectionMat * modelViewMat * vec4( position, 1.0 );
