dff97033c6a013d6f16c5013bafad17b8ba2ac10
Kevin Tan <33914892+kevtan@users.noreply.github.com>
2022-04-14 12:45:14 -0700

    🔊 Make CLI output prettier

--- homework2-misc/main.py
+++ homework2-misc/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from termcolor import cprint
 
 WIDTH = 5
 HEIGHT = 6
@@ -45,20 +45,25 @@ def main():
     v2_window = (0.5, 1, 0.5)
     v3_window = (4.5, 0.5, 0.3)
     # Vertices in NDC
+    cprint("Normalized Device Coordinates (NDC)", attrs=["dark", "underline"])
     v1_ndc = window_to_ndc(v1_window, WIDTH, HEIGHT)
     v2_ndc = window_to_ndc(v2_window, WIDTH, HEIGHT)
     v3_ndc = window_to_ndc(v3_window, WIDTH, HEIGHT)
     print(f"v1_ndc: {v1_ndc}")
     print(f"v2_ndc: {v2_ndc}")
     print(f"v3_ndc: {v3_ndc}")
+    print()
     # Vertices in clip space
+    cprint("Clip Space", attrs=["dark", "underline"])
     v1_clip = ndc_to_clip(v1_ndc, T_1, T_2, E_1)
     v2_clip = ndc_to_clip(v2_ndc, T_1, T_2, E_1)
     v3_clip = ndc_to_clip(v3_ndc, T_1, T_2, E_1)
     print(f"v1_clip: {v1_clip}")
     print(f"v2_clip: {v2_clip}")
     print(f"v3_clip: {v3_clip}")
+    print()
     # Vertices in view space
+    cprint("View Space", attrs=["dark", "underline"])
     v1_view = clip_to_view(v1_clip, PROJECTION_MATRIX)
     v2_view = clip_to_view(v2_clip, PROJECTION_MATRIX)
     v3_view = clip_to_view(v3_clip, PROJECTION_MATRIX)
